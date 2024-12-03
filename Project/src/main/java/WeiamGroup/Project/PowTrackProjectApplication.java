19d6a16a3e99152e480bd433c1de4048256d357a
Weiam1 <145970015+Weiam1@users.noreply.github.com>
2024-12-03 19:39:27 +0100

    Configuration
    Log-In info encryption and configuration

---
+++ Project/src/main/java/WeiamGroup/Project/PowTrackProjectApplication.java
@@ -0,0 +1,13 @@
+package WeiamGroup.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowTrackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowTrackProjectApplication.class, args);
+	}
+
+}
---
+++ Project/src/main/java/WeiamGroup/Project/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package WeiamGroup.Project.repository;
+
+import WeiamGroup.Project.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+    Optional<User> findByEmail (String email);
+
+    Optional<User> findByVerificationCode(String verificationCode);
+
+
+}
